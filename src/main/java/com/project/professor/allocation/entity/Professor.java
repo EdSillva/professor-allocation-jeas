06c0499bdd31640654ebd989cb9ed53360a4028a
Edson Silva <esdev.tec23@gmail.com>
2022-01-13 19:34:14 -0300

    Criação de classes da camada de modelo

---
+++ src/main/java/com/project/professor/allocation/entity/Professor.java
@@ -0,0 +1,40 @@
+package com.project.professor.allocation.entity;
+
+public class Professor {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    private Long id;
+    private String name;
+    private String cpf;
+    private Long departmentId;
+}
