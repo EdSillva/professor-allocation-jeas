9f6f5f3c1122af2c5011b72a46f87178604d340e
Edson Silva <esdev.tec23@gmail.com>
2022-01-25 19:32:56 -0300

    Formatando e deixando tudo organizado da camada de Repositório

--- src/main/java/com/project/professor/allocation/repository/ProfessorRepository.java
+++ src/main/java/com/project/professor/allocation/repository/ProfessorRepository.java
@@ -1,6 +1,5 @@
 package com.project.professor.allocation.repository;
 
-import com.project.professor.allocation.entity.Allocation;
 import com.project.professor.allocation.entity.Professor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
