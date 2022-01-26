a4d057e8225dff90da24f71d0e40ff9146d78a3f
Edson Silva <esdev.tec23@gmail.com>
2022-01-26 19:35:24 -0300

    Criação dos construtores das classes serviços

--- src/main/java/com/project/professor/allocation/service/CourseService.java
+++ src/main/java/com/project/professor/allocation/service/CourseService.java
@@ -1,2 +1,14 @@
-package com.project.professor.allocation.service;public class CourseService {
+package com.project.professor.allocation.service;
+
+import com.project.professor.allocation.repository.CourseRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+
+    private CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
 }
--- src/main/java/com/project/professor/allocation/service/ProfessorService.java
+++ src/main/java/com/project/professor/allocation/service/ProfessorService.java
@@ -1,2 +1,14 @@
-package com.project.professor.allocation.service;public class ProfessorService {
+package com.project.professor.allocation.service;
+
+import com.project.professor.allocation.repository.ProfessorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProfessorService {
+
+    private ProfessorRepository professorRepository;
+
+    public ProfessorService(ProfessorRepository professorRepository) {
+        this.professorRepository = professorRepository;
+    }
 }
